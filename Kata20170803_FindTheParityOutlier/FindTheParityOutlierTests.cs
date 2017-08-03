2e32cae2c67d4c6176c350e1503042fbd4ddcc30
cash <cash@cashwu.com>
2017-08-03 19:56:15 +0800

    add codewars test case

--- Kata20170803_FindTheParityOutlier/FindTheParityOutlierTests.cs
+++ Kata20170803_FindTheParityOutlier/FindTheParityOutlierTests.cs
@@ -20,6 +20,24 @@ namespace Kata20170803_FindTheParityOutlier
             AssertParityOutlierShouldbe(3, new[] { 2, 3, 4 });
         }
 
+        [TestMethod]
+        public void input_2_6_8_n10_3_should_return_3()
+        {
+            AssertParityOutlierShouldbe(3, new [] { 2, 6, 8, -10, 3 });
+        }
+
+        [TestMethod]
+        public void input_intMax_0_1_should_return_0()
+        {
+            AssertParityOutlierShouldbe(0, new [] { int.MaxValue, 0, 1 });
+        }
+
+        [TestMethod]
+        public void input_206847684_1056521_7_17_1901_21104421_7_1_35521_1_7781_should_return_206847684()
+        {
+            AssertParityOutlierShouldbe(206847684, new []{ 206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781 });
+        }
+
         private static void AssertParityOutlierShouldbe(int expected, int[] integers)
         {
             var kata = new Kata();
