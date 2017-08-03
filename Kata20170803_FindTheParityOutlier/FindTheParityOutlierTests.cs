14c838ad3d7c8f9a4623621c275ee7aacbc37c03
cash <cash@cashwu.com>
2017-08-03 19:51:42 +0800

    input 2 , 3, 4 test case

--- Kata20170803_FindTheParityOutlier/FindTheParityOutlierTests.cs
+++ Kata20170803_FindTheParityOutlier/FindTheParityOutlierTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170803_FindTheParityOutlier
@@ -8,9 +9,15 @@ namespace Kata20170803_FindTheParityOutlier
     {
         //https://www.codewars.com/kata/5526fc09a1bbd946250002dc
         [TestMethod]
-        public void input_1_3_2()
+        public void input_1_3_2_should_return_2()
         {
             AssertParityOutlierShouldbe(2, new[] {1, 3, 2});
+        }
+
+        [TestMethod]
+        public void input_2_3_4_should_return_3()
+        {
+            AssertParityOutlierShouldbe(3, new[] { 2, 3, 4 });
         }
 
         private static void AssertParityOutlierShouldbe(int expected, int[] integers)
@@ -25,7 +32,13 @@ namespace Kata20170803_FindTheParityOutlier
     {
         public int Find(int[] integers)
         {
-            return 2;
+            var evenList = integers.Where(a => a % 2 == 0).ToList();
+            if (evenList.Count == 1)
+            {
+                return evenList[0];
+            }
+
+            return integers.FirstOrDefault(num => num % 2 != 0);
         }
     }
 }
