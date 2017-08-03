2a13a9bc7e96cc9896d49fc096fd915e1a4846f8
cash <cash@cashwu.com>
2017-08-03 12:40:59 +0800

    init

--- Kata20170803_FindTheParityOutlier/FindTheParityOutlierTests.cs
+++ Kata20170803_FindTheParityOutlier/FindTheParityOutlierTests.cs
@@ -4,8 +4,9 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170803_FindTheParityOutlier
 {
     [TestClass]
-    public class UnitTest1
+    public class FindTheParityOutlierTests
     {
+        //https://www.codewars.com/kata/5526fc09a1bbd946250002dc
         [TestMethod]
         public void TestMethod1()
         {
